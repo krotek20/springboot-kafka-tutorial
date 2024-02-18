d454cdf48b7f7010f77b652c33eae971a006d54e
Vintila Radu <radu_vintila20@yahoo.com>
2024-02-18 14:18:11 +0200

    first commit

---
+++ src/test/java/com/soatutorial/springbootkafkatutorial/SpringbootKafkaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soatutorial.springbootkafkatutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
